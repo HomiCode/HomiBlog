e7a5e1febe61c8152ad22352d2e9103c43756b89
Amir Homayoon <amirhomayoonrefaei@gmail.com>
2023-01-30 09:02:50 +0330

    Debug and better url

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('<int:id>/<int:month>/<int:day>/<slug:post>/',
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post,
+                             publish__year=year, publish__month=month, publish__day=day,)
 
     return render(request, 'blog/post/detail.html', {'post': post})
 
