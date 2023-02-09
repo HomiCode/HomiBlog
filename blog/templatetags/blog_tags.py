bcabc9246050a024bb28a1d2671ff204482d55b8
Amir Homayoon <amirhomayoonrefaei@gmail.com>
2023-02-09 06:46:59 +0330

    Adding Post w/ Markdown

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,7 @@
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 from ..models import Post
 
 register = template.Library()
@@ -18,4 +20,9 @@ def show_latest_posts(count=5):
 
 @register.simple_tag
 def get_most_commented_posts(count=5):
-    return Post.published.annotate(total_comments=count('comments')).order_by('-total_comments')[:count]
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
