18b39d53655dc7516136d0225326c42789efe84c
Amir Homayoon <amirhomayoonrefaei@gmail.com>
2023-02-09 06:49:47 +0330

    Settings Update

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -41,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
