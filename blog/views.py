ac0df3c65fdc091b89c7d223716264a8af8bee0f
Amir Homayoon <amirhomayoonrefaei@gmail.com>
2023-02-09 06:12:04 +0330

    Taggit #Finish

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
-from django.views.generic import ListView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
 from django.views.decorators.http import require_POST
 
 
@@ -15,11 +16,24 @@ def post_detail(request, year, month, day, post):
     return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts,
+                   'tag': tag})
 
 
 def post_share(request, post_id):
